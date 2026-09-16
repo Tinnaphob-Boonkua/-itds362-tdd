becbc57ae03946947a4e54b6eb43591e5f762c20
Tinnaphob-Boonkua <markzabk@gmail.com>
2026-09-16 21:20:58 +0700

    First green, and then Red from changing test without change kitchen "hard-coded"

--- test_kitchen.py
+++ test_kitchen.py
@@ -7,10 +7,15 @@
 # ☐ 200 g + 1 oz, reduced to grams, using a conversion rate
 # ☐ (200 g + 1 oz) × 2
 
-from kitchen import Quantitys
+from kitchen import Quantity
 
 
 def test_multiplication():
     flour = Quantity(200)
     flour.times(3)
     assert flour.amount == 600
+
+def test_multiplication_by_two():
+    flour = Quantity(200)
+    flour.times(2)
+    assert flour.amount == 400
