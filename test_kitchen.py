4c07e55c70ada35f0d1f603e840963d583e6c819
Tinnaphob-Boonkua <markzabk@gmail.com>
2026-09-16 22:38:21 +0700

    5A Red Bar: Grams are not ounces

--- test_kitchen.py
+++ test_kitchen.py
@@ -21,5 +21,9 @@ def test_multiplication_returns_a_new_quantity():
     assert flour.times(2) == Quantity(400)
 
 def test_equality():
- assert Quantity(200) == Quantity(200)
- assert Quantity(200) != Quantity(300)
+    assert Quantity(200) == Quantity(200)
+    assert Quantity(200) != Quantity(300)
+
+
+def test_grams_are_not_ounces():
+    assert Quantity(1, "g") != Quantity(1, "oz")
